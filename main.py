917663e5e7236e1aa3ccc0bc71c040b328359b9a
koby <jacov.g@gmail.com>
2022-06-14 18:53:26 +0300

    Not Finished Yet.

--- main.py
+++ main.py
@@ -75,16 +75,17 @@ def red_handle_movement(key_pressed, red):
         red.y += VEL
 
 
-def handle_bullets(yellow_bullets, red_bullets, yellow. red):
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
     for bullet in yellow_bullets:
         bullet.x += BULLET_VEL
         if red.colliderect(bullet):
-
+            yellow_bullets.remove(bullet)
         elif bullet.x > WIDTH:
             yellow_bullets.remove(bullet)
     for bullet in red_bullets:
         bullet.x -= BULLET_VEL
         if yellow.colliderect(bullet):
+            red_bullets.remove(bullet)
         elif bullet.x < 0:
             red_bullets.remove(bullet)
 

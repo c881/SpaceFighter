68a869a2017f8edf08ad2e9fda3ea873221ee465
koby <jacov.g@gmail.com>
2022-06-15 16:28:46 +0300

    Not Finished Yet.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pygame as pg
 
+
+p = Path(__file__).parent
 # Window
 WIDTH, HEIGHT = 600, 400
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
@@ -9,6 +11,7 @@ pg.font.init()
 pg.mixer.init()
 pg.display.set_caption("RPG with Nimrod")
 HEALTH_FONT = pg.font.SysFont('arial', 40)
+WINNER_FONT = pg.font.SysFont('comicsans', 100)
 # Game speed
 FPS = 60
 
@@ -20,8 +23,8 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 BULLET_VEL = 10
 MAX_BULLETS = 5
 BULLET_WIDTH, BULLET_HEIGHT = 5, 10
-BULLET_FIRE_SOUND = pg.mixer.load(p.joinpath('Assets','Gun+Silence.mp3')
-BULLET_HIT_SOUND = pg.mixer.load(p.joinpath('Assets','Granede+1.mp3')
+BULLET_FIRE_SOUND = pg.mixer.Sound('Assets/game-gun-shot.mp3')
+BULLET_HIT_SOUND = pg.mixer.Sound('Assets/explosion-01.mp3')
 
 YELLOW_HIT = pg.USEREVENT + 1
 RED_HIT = pg.USEREVENT + 2
@@ -29,18 +32,18 @@ RED_HIT = pg.USEREVENT + 2
 # Colors
 WHITE = (200, 200, 255)
 BLACK = (0, 0, 0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
-p = Path(__file__).parent
 
 # We can use os.path.join because of different os use different writing
 # We use pathlib, because we are only dealing with Paths
 # YELLOW_SPACESHIP = pg.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 
 class Spaceship:
-    def __init__(image_path, width, height, direction, health, bullets=None):
-        self.image = pg.transform.rotate(pg.transform.scale(pg.image.load(image_path),(width, height)),direction)
+    def __init__(self, image_path, width, height, direction, health, bullets=None):
+        self.image = pg.transform.rotate(pg.transform.scale(pg.image.load(image_path),
+                                                            (width, height)), direction)
         self.width = width
         self.height = height
         self.health = health
@@ -56,13 +59,14 @@ RED_SPACESHIP = pg.transform.rotate(pg.transform.scale(
 SPACE = pg.transform.scale(
     pg.image.load(p.joinpath('Assets', 'space.png')), (WIDTH, HEIGHT))
 
+
 def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
     # WIN.fill(WHITE)
-    WIN.blit(SPACE,(0,0))
+    WIN.blit(SPACE, (0, 0))
     pg.draw.rect(WIN, BLACK, BORDER)
-    red_health_text = HEALTH_FONT.render(f'Health:{red_health}',1,WHITE)
-    yellow_health_text = HEALTH_FONT.render(f'Health:{yellow_health}',1,WHITE)
-    WIN.blit(red_health_text, (WIDTH-red_health_text.get_width()-10,10))
+    red_health_text = HEALTH_FONT.render(f'Health:{red_health}', 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render(f'Health:{yellow_health}', 1, WHITE)
+    WIN.blit(red_health_text, (WIDTH-red_health_text.get_width()-10, 10))
     WIN.blit(yellow_health_text, (10, 10))
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
@@ -114,6 +118,15 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
             red_bullets.remove(bullet)
 
 
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    x = WIDTH/2 - draw_text.get_width()/2
+    y = HEIGHT/2 - draw_text.get_height()/2
+    WIN.blit(draw_text,( x, y))
+    pg.display.update()
+    pg.time.delay(5000)
+
+
 # Dealing with the logic of the game
 def main():
     run = True
@@ -136,9 +149,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
+                pg.quit()
+
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                    bullet = pg.Rect(yellow.x + 5 + SPACESHIP_WIDTH, yellow.y + SPACESHIP_HEIGHT // 2, BULLET_WIDTH, BULLET_HEIGHT)
+                    bullet = pg.Rect(yellow.x + 5 + SPACESHIP_WIDTH, yellow.y + SPACESHIP_HEIGHT // 2,
+                                     BULLET_WIDTH, BULLET_HEIGHT)
                     yellow_bullets.append(bullet)
                     BULLET_FIRE_SOUND.play()
                 if event.key == pg.K_RCTRL and len(red_bullets) < MAX_BULLETS:
@@ -154,8 +170,8 @@ def main():
         if yellow_health < 1:
             winner_text = "Red Wins!"
         if winner_text != "":
-            run = False
-        
+            draw_winner(winner_text)
+            break
 
         key_pressed = pg.key.get_pressed()
         yellow_handle_movement(key_pressed, yellow)
@@ -163,6 +179,7 @@ def main():
         handle_bullets(yellow_bullets, red_bullets, yellow, red)
 
         draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
+    main()
 
 
 if __name__ == "__main__":
